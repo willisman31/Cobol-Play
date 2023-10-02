28edefe5e679a7c98a88a4594badc785fafee44b
willisman31 <jake22032@gmail.com>
2023-10-01 23:38:07 -0400

    Create HelloWorld and Fibonacci programs

---
+++ helloworld.cbl
@@ -0,0 +1,11 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.  HELLO-WORLD.
+      * simple helloworld program.
+       PROCEDURE DIVISION.
+           DISPLAY '###################'.
+           DISPLAY '#!!!!!!!!!!!!!!!!!#'.
+           DISPLAY '#!!!HELLO WORLD!!!#'.
+           DISPLAY '#!!!!!!!!!!!!!!!!!#'.
+           DISPLAY '###################'.
+       STOP RUN.
+
